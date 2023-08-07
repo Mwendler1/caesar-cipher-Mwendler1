411aa9cebc0a5f43e3a46049db42d52039437583
Matthew Wendler <102497931+Mwendler1@users.noreply.github.com>
2023-08-06 21:04:22 -0400

    Update cipher.py

--- cipher.py
+++ cipher.py
@@ -1,1 +1,15 @@
-# add your code here
+import string
+
+text = input("Please Enter a Statement to Be Encrypted: ")
+text = text.lower()
+shift = 5
+
+
+alpha = string.ascii_lowercase
+shifted = alpha[shift:] + alpha[:shift]
+table = str.maketrans(alpha, shifted)
+
+encrypted = text.translate(table)
+
+print(encrypted)
+
